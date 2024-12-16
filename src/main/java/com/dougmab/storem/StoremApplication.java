5a9ca845f06a2efc4c89cc49afb7028a2737b9d7
Douglas Brum <douglas.mateus2005@gmail.com>
2024-12-16 11:35:58 -0300

    first commit

---
+++ src/main/java/com/dougmab/storem/StoremApplication.java
@@ -0,0 +1,13 @@
+package com.dougmab.storem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoremApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dougmab/storem/StoremApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougmab.storem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
